538d28d49faa283a72fd0b7166a8e99ff030d23d
ThanhTran2k1 <tranquocthanhd19ptiter@gmail.com>
2022-09-27 01:03:30 +0700

    completeDeepSleep

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,9 @@
 const char* ssid     = "P307";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "bayanhdeptrai";     // The password of the Wi-Fi network
 
-int led = D4;
 int ledWifiStatus = D0;
-int led1 = D2;
-int led2 = D3;
-int i=0;
-unsigned long preMillisForLed1  = 0;
-unsigned long preMillisForLed2  = 0;
+int pumpPush = D1; 
+
 unsigned long currentMillisWifi = 0;
 unsigned long currentMillis = millis();
 unsigned long led1Delay = 2000;
@@ -25,41 +21,38 @@ unsigned long led2Delay = 3000;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-char hour[5];
+bool onPumps = false;
+int i=0;
 
+void handlerPumps() {
+  digitalWrite(pumpPush, LOW);
+  delay(4000);
+  Serial.print(++i);
+  digitalWrite(pumpPush,HIGH);
+}
 
-// void handlerPumps() {
-//   digitalWrite(led, LOW);
-//   delaymillis(1000);  
-//   digitalWrite(led, HIGH);
-//   delaymillis(1000);
-// }
-
-// void handleTime() {
-//   String formattedTime = timeClient.getFormattedTime();
-//   hour[0] = '2';
-//   hour[1] = '0';
-//   Serial.print("Formatted Time: ");
-//   Serial.println(formattedTime);
-//   if (hour[0] == formattedTime[0] && hour[1] == formattedTime[1]){
-//     handlerPumps();   
-//   }
-//   delaymillis(2000);
-// }
+String timeSet = "17:50";
+String convert;
+void handleTime() {
+  String formattedTime = timeClient.getFormattedTime(); 
+  String num = formattedTime.substring(0,5);
+  Serial.println(num);
+  if(num == timeSet) {
+    Serial.println("YES");
+  }else {
+    Serial.println("NO");
+  }
+}
 
 bool isConnected = false;
-
+unsigned long previosTime = 0;
 void setup() {
   Serial.begin(115200); 
-  WiFi.begin(ssid, password);        // Start the Serial communication to send messages to the computer
-  pinMode(ledWifiStatus, OUTPUT);
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  
+  WiFi.begin(ssid, password);
+  pinMode(pumpPush, OUTPUT);
   timeClient.begin();
   timeClient.setTimeOffset(25200);
+  
 }
 
 void loop() {
@@ -76,25 +69,10 @@ void loop() {
     }
     isConnected = false;
   }
-  
-  currentMillis = millis();
-  // led1 delay with 2000ms
-  if(currentMillis - preMillisForLed1 >= 1000) {
-    preMillisForLed1 = millis();
-    if(digitalRead(led1) == LOW) {
-      digitalWrite(led1, HIGH);
-    } 
-    else digitalWrite(led1, LOW);
-  
+  if(millis() - previosTime >= 1000) {
+    previosTime = millis();
+    handleTime();
   }
-  // led2 delay with 3000ms
-  if(millis() - preMillisForLed2 >= 2000) {
-    preMillisForLed2 = millis();
-    if(digitalRead(led2) == LOW) {
-      digitalWrite(led2, HIGH);
-    } 
-    else digitalWrite(led2, LOW);
   
-  }
-  // sdhasjhdkjasdghdash
+  
 }
